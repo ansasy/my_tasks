72714d4bc04128555d9d00ce21da436317220373
abdelrahman elansasy <aelansasy@gmail.com>
2023-09-01 05:21:36 +0300

    task 1 added

---
+++ 01124005422_task1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+printf("GRU\n");
+
+printf("\
+         *****   ****    *    *\n\
+         *       *   *   *    *\n\
+         *  **   ****    *    *\n\
+         *   *   *   *   *    *\n\
+         *****   *    *  ******\n   ");
+}
